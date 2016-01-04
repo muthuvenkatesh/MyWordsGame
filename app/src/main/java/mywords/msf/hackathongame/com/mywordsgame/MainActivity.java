39d735462f9591c3425201a66c727206d0438384
unknown <muthuv@muthuvenkatesh_Laptop.msf.ad>
2016-01-04 13:13:23 +0530

    Loaded level 1

--- app/src/main/java/mywords/msf/hackathongame/com/mywordsgame/MainActivity.java
+++ app/src/main/java/mywords/msf/hackathongame/com/mywordsgame/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.ContextWrapper;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,19 +14,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
     private DBHelper dbHelper;
-    private Button btn ;
-    List<String> questionList = new ArrayList<>();
+    private Button btn, loadlevel1 ;
+    private List<String> questionList = new ArrayList<>();
+    private TextView loadingtxt;
+    private LinearLayout level1Layout;
     private String TAG = "MyWordsGame";
 
     @Override
@@ -33,26 +40,60 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = (Button)findViewById(R.id.btn);
+        loadlevel1 =  (Button)findViewById(R.id.loadlevel1);
+        level1Layout = (LinearLayout)findViewById(R.id.level1Layout);
+        loadingtxt = (TextView)findViewById(R.id.loadingtxt);
+
         dbHelper = new DBHelper(this);
         Log.i(TAG, String.valueOf(doesDatabaseExist(this, "MyDBName.db")));
         String chkDb =  String.valueOf(doesDatabaseExist(this, "MyDBName.db"));
         if (!chkDb.equalsIgnoreCase("true")) {
             copyTextFileToDB();
         }
-        //copyTextFileToDB();
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*List<String> chk = new ArrayList<String>();
                 chk = dbHelper.getAllComments();
                 Log.i("chk Count=====", String.valueOf(chk));*/
-                questionList = new ArrayList<String>();
-                Cursor cd = dbHelper.getAllComments();
-                for (cd.moveToFirst(); !(cd.isAfterLast()); cd.moveToNext()) {
-                    String userCount = cd.getString(cd.getColumnIndex("name"));
-                    questionList.add(userCount);
-                }
+                //if (questionList.size() == 0){
+                    Cursor cd = dbHelper.getAllComments();
+                    for (cd.moveToFirst(); !(cd.isAfterLast()); cd.moveToNext()) {
+                        String userCount = cd.getString(cd.getColumnIndex("name"));
+                        questionList.add(userCount);
+                    }
+                //}
+                if (questionList.size() > 0){
+                    loadlevel1.setVisibility(View.VISIBLE);
+                    loadingtxt.setVisibility(View.GONE);
+                } /*else {
+                    loadlevel1.setVisibility(View.GONE);
+                    loadingtxt.setVisibility(View.VISIBLE);
+                }*/
+                //loadlevel1.setVisibility(View.VISIBLE);
                 Log.i(TAG, String.valueOf(questionList.size()));
+            }
+        });
+
+        loadlevel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random rand = new Random();
+                level1Layout.removeAllViews();
+                final int N = 10;
+                final TextView[] myTextViews = new TextView[N];
+                for (int i =1; i<=5; i++){
+                    String randomInt = questionList.get(rand.nextInt(questionList.size()));
+                    Log.i(TAG, "Data========== "+ i +"====="+ randomInt);
+                    final TextView rowTextView = new TextView(MainActivity.this);
+                    rowTextView.setText( i + ")" + randomInt);
+                    rowTextView.setTextColor(Color.BLACK);
+                    rowTextView.setTextSize(20);
+                    level1Layout.addView(rowTextView);
+                    myTextViews[i] = rowTextView;
+                }
             }
         });
     }
